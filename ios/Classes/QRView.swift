c32da93bc1cf1927669daf5f8ff35303b58a519c
Sanju Verma <sv@hathtech.com>
2020-09-02 12:12:59 +0530

    Added the fix for black screen

---
+++ ios/Classes/QRView.swift
@@ -0,0 +1,138 @@
+//
+//  QRView.swift
+//  flutter_qr
+//
+//  Created by Julius Canute on 21/12/18.
+//
+
+import Foundation
+import MTBBarcodeScanner
+
+public class QRView:NSObject,FlutterPlatformView {
+    @IBOutlet var previewView: UIView!
+    var scanner: MTBBarcodeScanner?
+    var registrar: FlutterPluginRegistrar
+    var channel: FlutterMethodChannel
+    
+    public init(withFrame frame: CGRect, withRegistrar registrar: FlutterPluginRegistrar, withId id: Int64){
+        self.registrar = registrar
+        previewView = UIView(frame: frame)
+        channel = FlutterMethodChannel(name: "net.touchcapture.qr.flutterqr/qrview_\(id)", binaryMessenger: registrar.messenger())
+    }
+    
+    func isCameraAvailable(success: Bool) -> Void {
+        if success {
+            do {
+                try scanner?.startScanning(resultBlock: { codes in
+                    if let codes = codes {
+                        for code in codes {
+                            var typeString: String;
+                            switch(code.type) {
+                                case AVMetadataObject.ObjectType.aztec:
+                                   typeString = "AZTEC"
+                                case AVMetadataObject.ObjectType.code39:
+                                    typeString = "CODE_39"
+                                case AVMetadataObject.ObjectType.code93:
+                                    typeString = "CODE_93"
+                                case AVMetadataObject.ObjectType.code128:
+                                    typeString = "CODE_128"
+                                case AVMetadataObject.ObjectType.dataMatrix:
+                                    typeString = "DATA_MATRIX"
+                                case AVMetadataObject.ObjectType.ean8:
+                                    typeString = "EAN_8"
+                                case AVMetadataObject.ObjectType.ean13:
+                                    typeString = "EAN_13"
+                                case AVMetadataObject.ObjectType.itf14:
+                                    typeString = "ITF"
+                                case AVMetadataObject.ObjectType.pdf417:
+                                    typeString = "PDF_417"
+                                case AVMetadataObject.ObjectType.qr:
+                                    typeString = "QR_CODE"
+                                case AVMetadataObject.ObjectType.upce:
+                                    typeString = "UPC_E"
+                                default:
+                                    return
+                            }
+
+                            guard let stringValue = code.stringValue else { continue }
+                            let result = ["code": stringValue, "type": typeString]
+                            self.channel.invokeMethod("onRecognizeQR", arguments: result)
+                        }
+                    }
+                })
+            } catch {
+                NSLog("Unable to start scanning")
+            }
+        } else {
+            UIAlertView(title: "Scanning Unavailable", message: "This app does not have permission to access the camera", delegate: nil, cancelButtonTitle: nil, otherButtonTitles: "Ok").show()
+        }
+    }
+    
+    public func view() -> UIView {
+        channel.setMethodCallHandler({
+            [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+            switch(call.method){
+                case "setDimensions":
+                    var arguments = call.arguments as! Dictionary<String, Double>
+                    self?.setDimensions(width: arguments["width"] ?? 0,height: arguments["height"] ?? 0)
+                case "flipCamera":
+                    self?.flipCamera()
+                case "toggleFlash":
+                    self?.toggleFlash()
+                case "pauseCamera":
+                    self?.pauseCamera()
+                case "resumeCamera":
+                    self?.resumeCamera()
+                default:
+                    result(FlutterMethodNotImplemented)
+                    return
+            }
+        })
+        return previewView
+    }
+    
+    func setDimensions(width: Double, height: Double) -> Void {
+       previewView.frame = CGRect(x: 0, y: 0, width: width, height: height)
+
+        if let sc: MTBBarcodeScanner = scanner {
+            if let previewLayer = sc.previewLayer {
+                previewLayer.frame = previewView.bounds;
+            }
+        } else {
+            scanner = MTBBarcodeScanner(previewView: previewView)
+            MTBBarcodeScanner.requestCameraPermission(success: isCameraAvailable)
+        }
+    }
+    
+    func flipCamera(){
+        if let sc: MTBBarcodeScanner = scanner {
+            if sc.hasOppositeCamera() {
+                sc.flipCamera()
+            }
+        }
+    }
+    
+    func toggleFlash(){
+        if let sc: MTBBarcodeScanner = scanner {
+            if sc.hasTorch() {
+                sc.toggleTorch()
+            }
+        }
+    }
+    
+    func pauseCamera() {
+        if let sc: MTBBarcodeScanner = scanner {
+            if sc.isScanning() {
+                sc.freezeCapture()
+            }
+        }
+    }
+    
+    func resumeCamera() {
+        if let sc: MTBBarcodeScanner = scanner {
+            if !sc.isScanning() {
+                sc.unfreezeCapture()
+            }
+        }
+    }
+}
---
+++ lib/src/qr_code_scanner.dart
@@ -0,0 +1,238 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+typedef QRViewCreatedCallback = void Function(QRViewController);
+
+enum BarcodeFormat {
+  /// Aztec 2D barcode format.
+  aztec,
+
+  /// CODABAR 1D format.
+  codabar,
+
+  /// Code 39 1D format.
+  code39,
+
+  /// Code 93 1D format.
+  code93,
+
+  /// Code 128 1D format.
+  code128,
+
+  /// Data Matrix 2D barcode format.
+  dataMatrix,
+
+  /// EAN-8 1D format.
+  ean8,
+
+  /// EAN-13 1D format.
+  ean13,
+
+  /// ITF (Interleaved Two of Five) 1D format.
+  itf,
+
+  /// MaxiCode 2D barcode format.
+  maxicode,
+
+  /// PDF417 format.
+  pdf417,
+
+  /// QR Code 2D barcode format.
+  qrcode,
+
+  /// RSS 14
+  rss14,
+
+  /// RSS EXPANDED
+  rssExpanded,
+
+  /// UPC-A 1D format.
+  upcA,
+
+  /// UPC-E 1D format.
+  upcE,
+
+  /// UPC/EAN extension format. Not a stand-alone format.
+  upcEanExtension
+}
+
+const _formatNames = <String, BarcodeFormat>{
+  'AZTEC': BarcodeFormat.aztec,
+  'CODABAR': BarcodeFormat.codabar,
+  'CODE_39': BarcodeFormat.code39,
+  'CODE_93': BarcodeFormat.code93,
+  'CODE_128': BarcodeFormat.code128,
+  'DATA_MATRIX': BarcodeFormat.dataMatrix,
+  'EAN_8': BarcodeFormat.ean8,
+  'EAN_13': BarcodeFormat.ean13,
+  'ITF': BarcodeFormat.itf,
+  'MAXICODE': BarcodeFormat.maxicode,
+  'PDF_417': BarcodeFormat.pdf417,
+  'QR_CODE': BarcodeFormat.qrcode,
+  'RSS_14': BarcodeFormat.rss14,
+  'RSS_EXPANDED': BarcodeFormat.rssExpanded,
+  'UPC_A': BarcodeFormat.upcA,
+  'UPC_E': BarcodeFormat.upcE,
+  'UPC_EAN_EXTENSION': BarcodeFormat.upcEanExtension,
+};
+
+class Barcode {
+  Barcode(this.code, this.format);
+
+  final String code;
+  final BarcodeFormat format;
+}
+
+class QRView extends StatefulWidget {
+  const QRView({
+    @required Key key,
+    @required this.onQRViewCreated,
+    this.overlay,
+  })  : assert(key != null),
+        assert(onQRViewCreated != null),
+        super(key: key);
+
+  final QRViewCreatedCallback onQRViewCreated;
+
+  final ShapeBorder overlay;
+
+  @override
+  State<StatefulWidget> createState() => _QRViewState();
+}
+
+class _QRViewState extends State<QRView> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        _getPlatformQrView(),
+        if (widget.overlay != null)
+          Container(
+            decoration: ShapeDecoration(
+              shape: widget.overlay,
+            ),
+          )
+        else
+          Container(),
+      ],
+    );
+  }
+
+  Widget _getPlatformQrView() {
+    Widget _platformQrView;
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        _platformQrView = AndroidView(
+          viewType: 'net.touchcapture.qr.flutterqr/qrview',
+          onPlatformViewCreated: _onPlatformViewCreated,
+        );
+        break;
+      case TargetPlatform.iOS:
+        _platformQrView = UiKitView(
+          viewType: 'net.touchcapture.qr.flutterqr/qrview',
+          onPlatformViewCreated: _onPlatformViewCreated,
+          creationParams: _CreationParams.fromWidget(0, 0).toMap(),
+          creationParamsCodec: StandardMessageCodec(),
+        );
+        break;
+      default:
+        throw UnsupportedError(
+            "Trying to use the default webview implementation for $defaultTargetPlatform but there isn't a default one");
+    }
+    return _platformQrView;
+  }
+
+  void _onPlatformViewCreated(int id) {
+    if (widget.onQRViewCreated == null) {
+      return;
+    }
+    widget.onQRViewCreated(QRViewController._(id, widget.key));
+  }
+}
+
+class _CreationParams {
+  _CreationParams({this.width, this.height});
+
+  static _CreationParams fromWidget(double width, double height) {
+    return _CreationParams(
+      width: width,
+      height: height,
+    );
+  }
+
+  final double width;
+  final double height;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'width': width,
+      'height': height,
+    };
+  }
+}
+
+class QRViewController {
+  QRViewController._(int id, GlobalKey qrKey)
+      : _channel = MethodChannel('net.touchcapture.qr.flutterqr/qrview_$id') {
+    updateDimensions(qrKey);
+    _channel.setMethodCallHandler(
+      (call) async {
+        switch (call.method) {
+          case scanMethodCall:
+            if (call.arguments != null) {
+              final args = call.arguments as Map;
+              final code = args['code'] as String;
+              final rawType = args['type'] as String;
+              final format = _formatNames[rawType];
+              if (format != null) {
+                final barcode = Barcode(code, format);
+                _scanUpdateController.sink.add(barcode);
+              } else {
+                throw Exception('Unexpected barcode type $rawType');
+              }
+            }
+        }
+      },
+    );
+  }
+
+  static const scanMethodCall = 'onRecognizeQR';
+
+  final MethodChannel _channel;
+
+  final StreamController<Barcode> _scanUpdateController =
+      StreamController<Barcode>();
+
+  Stream<Barcode> get scannedDataStream => _scanUpdateController.stream;
+
+  void flipCamera() {
+    _channel.invokeMethod('flipCamera');
+  }
+
+  void toggleFlash() {
+    _channel.invokeMethod('toggleFlash');
+  }
+
+  void pauseCamera() {
+    _channel.invokeMethod('pauseCamera');
+  }
+
+  void resumeCamera() {
+    _channel.invokeMethod('resumeCamera');
+  }
+
+  void updateDimensions(GlobalKey key) {
+    if (defaultTargetPlatform == TargetPlatform.iOS) {
+      final RenderBox renderBox = key.currentContext.findRenderObject();
+      _channel.invokeMethod('setDimensions',
+          {'width': renderBox.size.width, 'height': renderBox.size.height});
+    }
+  }
+
+  void dispose() {
+    _scanUpdateController.close();
+  }
+}
